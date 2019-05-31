da38d127791ee8ce39eb629e6fc7aa86cc407829
CesarRivasP <cesarjose58@gmail.com>
2019-05-31 18:19:15 -0400

    Implemented body of appointment component

--- src/components/appointment.js
+++ src/components/appointment.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
-const Appointment = () => (
-  <div className="">
+const Appointment = ({ appointment }) => (
+  <div className="media mt-3">
+    <div className="media-body">
+      <h3 className="mt-0">{ appointment.pet }</h3>
 
+      <p className="card-text">
+        <span>Nombre del Dueño: </span>{ appointment.owner }
+      </p>
+      <p className="card-text">
+        <span>Fecha </span>{ appointment.date }
+      </p>
+      <p className="card-text">
+        <span>Hora: </span>{ appointment.hour }
+      </p>
+      <p className="card-text">
+        <span>Sintomas: </span>{ appointment.symptom }
+      </p>
+    </div>
   </div>
 );
 
