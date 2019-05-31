3fbbfc774b8c576be5b448aa1bb5fdde6b6a2fa0
CesarRivasP <cesarjose58@gmail.com>
2019-05-31 10:22:32 -0400

    Added handle chance funtion and values for the inputs. And send state

--- src/components/new-appoitment.js
+++ src/components/new-appoitment.js
@@ -1,6 +1,57 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
+
 
 class NewAppoitment extends Component {
+  state = {
+    appointment: {
+      pet: '',
+      owner: '',
+      date: '',
+      hour: '',
+      symptom: ''
+    },
+    error: false
+  }
+
+  petRef = React.createRef();
+  ownerRef = React.createRef();
+  dateRef = React.createRef();
+  hourRef = React.createRef();
+  symptomRef = React.createRef();
+
+
+  handleChange = (e) => {
+    this.setState({
+      appointment: {
+        ...this.state.appointment,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  // Cuando el usuario envia el formulario
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // extraer los valores del state
+    const { pet, owner, date, hour, symptom } = this.state.appointment;
+
+    // validar que todos los campos esten llenos
+    if(pet === '' || owner === '' || date === '' || hour === '' || symptom === ''){
+      this.setState({ error: true })
+      // Detener la ejecucion con un return
+      return;
+    }
+
+    // generar objeto con los datos
+    const newAppoitment = { ...this.state.appointment };
+    newAppoitment.id = uuid()
+
+    // agregar la cita al state de App.js
+    this.props.createNewAppointment(newAppoitment);
+  }
+
   render(){
     return (
       <div className="card mt-5 py-5">
@@ -9,39 +60,81 @@ class NewAppoitment extends Component {
             Llena el formulario para una nueva cita
           </h2>
 
-          <form>
-            <div className="form-group row">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group row pl-5">
               <label htmlFor="pet" className="col-sm-4 col-lg-2 col-form-label">Mascota</label>
-              <div className="col-sm-8 col-lg-9">
-                <input type="text" className="form-control" placeholder="Mascota" name="pet" id="pet"/>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label htmlFor="owner" className="col-sm-4 col-lg-3 col-form-label">Nombre del dueño</label>
               <div className="col-sm-8 col-lg-8">
-                <input type="text" className="form-control" placeholder="Nombre del dueño de la mascota" id="owner" />
+                <input
+                  ref={this.petRef}
+                  type="text"
+                  className="form-control"
+                  placeholder="Mascota"
+                  name="pet"
+                  id="pet"
+                  onChange={this.handleChange}
+                  value={this.state.appointment.pet}
+                />
               </div>
             </div>
-            <div className="form-group row">
+            <div className="form-group row pl-5">
+              <label htmlFor="owner" className="col-sm-4 col-lg-3 col-form-label">Nombre del dueño</label>
+              <div className="col-sm-8 col-lg-7">
+                <input
+                  ref={this.ownerRef}
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre del dueño de la mascota"
+                  name="owner"
+                  id="owner"
+                  onChange={this.handleChange}
+                  value={this.state.appointment.owner}
+                />
+              </div>
+            </div>
+            <div className="form-group row pl-5">
               <label htmlFor="date" className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
               <div className="col-sm-8 col-lg-3">
-                <input type="date" className="form-control" name="date" id="date"/>
+                <input
+                  ref={this.dateRef}
+                  type="date"
+                  className="form-control"
+                  name="date"
+                  id="date"
+                  onChange={this.handleChange}
+                  value={this.state.appointment.date}
+                />
               </div>
               <label htmlFor="hour" className="col-sm-4 col-lg-2 col-form-label">Hora</label>
               <div className="col-sm-8 col-lg-3">
-                <input type="time" className="form-control" name="hour" id="hour"/>
+                <input
+                  ref={this.hourRef}
+                  type="time"
+                  className="form-control"
+                  name="hour"
+                  id="hour"
+                  onChange={this.handleChange}
+                  value={this.state.appointment.hour}
+                />
               </div>
             </div>
-            <div className="form-group row">
+            <div className="form-group row pl-5">
               <label htmlFor="symptom" className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
               <div className="col-sm-8 col-lg-8">
-                <textarea className="form-control" placeholder="Describe los sintomas" name="symptom" id="symptom" cols="30" rows="3">
-
+                <textarea
+                  ref={this.symptomRef}
+                  className="form-control"
+                  placeholder="Describe los sintomas"
+                  name="symptom"
+                  id="symptom"
+                  cols="30"
+                  rows="3"
+                  onChange={this.handleChange}
+                  value={this.state.appointment.symptom} >
                 </textarea>
               </div>
             </div>
 
-            <input type="submit" value="Added new appointment" className="py-3 mt-2 btn btn-success"/>
+            <input type="submit" value="Nueva cita" className="py-1 mt-2 btn btn-success ml-5"/>
           </form>
         </div>
       </div>
@@ -49,4 +142,4 @@ class NewAppoitment extends Component {
   }
 }
 
-export default NewAppoitment
+export default NewAppoitment;
