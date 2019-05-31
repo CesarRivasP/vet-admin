6f402e33980cd97776b9d90d7a620a4af1127830
CesarRivasP <cesarjose58@gmail.com>
2019-05-31 18:18:52 -0400

    Implemented Appointment component

--- src/components/appointment-list.js
+++ src/components/appointment-list.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Appointment from './appointment';
 
-const AppointmentList = () => (
+const AppointmentList = ({ appointments }) => (
+  <div className="card mt-0 py-5 card-style">
+    <div className="card-body">
+      <h2 className="card-title text-center title-form">
+        Administra las citas aqui
+      </h2>
 
-)
+      <div className="lista-citas">
+        {
+          appointments.map((appointment) => (
+            <Appointment key={appointment.id} appointment={appointment} />
+          ))
+        }
+      </div>
+    </div>
+  </div>
+);
 
 export default AppointmentList;
