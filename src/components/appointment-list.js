a9c796f97d50c276ed110276b5f18ae1c6a42bd1
CesarRivasP <cesarjose58@gmail.com>
2019-05-31 20:49:14 -0400

    implemented proptypes and added divider

--- src/components/appointment-list.js
+++ src/components/appointment-list.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Divider from '@material-ui/core/Divider';
 import Appointment from './appointment';
 
 const AppointmentList = ({ appointments, deleteAppointment }) => (
   <div className="card mt-0 py-3 card-style">
-    <div className="card-body pl-10">
+    <div className="card-body pl-5">
       <h2 className="card-title text-center title-form">
         Administra las citas aqui
       </h2>
+      <Divider />
 
-      <div className="lista-citas">
+      <div className="lista-citas mt-4">
         {
           appointments.map((appointment) => (
             <Appointment key={appointment.id} appointment={appointment} deleteAppointment={deleteAppointment} />
@@ -18,5 +21,10 @@ const AppointmentList = ({ appointments, deleteAppointment }) => (
     </div>
   </div>
 );
+
+AppointmentList.propTypes = {
+  appointments: PropTypes.array.isRequired,
+  deleteAppointment: PropTypes.func.isRequired
+}
 
 export default AppointmentList;
