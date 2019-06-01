a51d4be555f71c24051b545a151d3f79120d0c6d
CesarRivasP <cesarjose58@gmail.com>
2019-05-31 21:12:48 -0400

    Added conditional message

--- src/components/appointment-list.js
+++ src/components/appointment-list.js
@@ -3,24 +3,30 @@ import PropTypes from 'prop-types';
 import Divider from '@material-ui/core/Divider';
 import Appointment from './appointment';
 
-const AppointmentList = ({ appointments, deleteAppointment }) => (
-  <div className="card mt-0 py-3 card-style">
-    <div className="card-body pl-5">
-      <h2 className="card-title text-center title-form">
-        Administra las citas aqui
-      </h2>
-      <Divider />
+const AppointmentList = ({ appointments, deleteAppointment }) => {
+  // Imprimir un mensaje con base a si hay citas o no
+  const message = Object.keys(appointments).length === 0 ? 'No hay citas' : 'Administra las citas aqui'
 
-      <div className="lista-citas mt-4">
-        {
-          appointments.map((appointment) => (
-            <Appointment key={appointment.id} appointment={appointment} deleteAppointment={deleteAppointment} />
-          ))
-        }
+  return (
+    <div className="card mt-0 py-3 card-style">
+      <div className="card-body pl-5">
+        <h2 className="card-title text-center title-form">
+          { message }
+        </h2>
+        <Divider />
+
+        <div className="lista-citas mt-4">
+          {
+            appointments.map((appointment) => (
+              <Appointment key={appointment.id} appointment={appointment} deleteAppointment={deleteAppointment} />
+            ))
+          }
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
+
 
 AppointmentList.propTypes = {
   appointments: PropTypes.array.isRequired,
