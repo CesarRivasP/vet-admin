3e2d92e3f4adf4450204c300c45f3e1d9672d7e6
CesarRivasP <cesarjose58@gmail.com>
2019-05-31 20:50:03 -0400

    implemented proptypes and added divider component

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Divider from '@material-ui/core/Divider';
+
 
 const Header = ({title}) => (
   <header>
-    <h1 className="text-center">{ title }</h1>
+    <h1 className="text-center m-0 mb-2">{ title }</h1>
+    <Divider style={{ backgroundColor: 'white' }}/>
   </header>
 );
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 export default Header;
